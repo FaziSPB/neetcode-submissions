45974aa262deb18110352e5b60b5b2af67388d2c
Szymon Banasiak <szymbana@gmail.com>
2026-08-25 12:28:46 +0200

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        double[][] cars = new double[position.length][2];
+        for (int i = 0; i < position.length; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = (double)(target - position[i])/speed[i];
+        }
+        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+        double[] stack = new double[position.length];
+        int top = -1;
+        for (double[] car: cars) {
+            if (top == -1 || car[1] > stack[top]) stack[++top] = car[1]; 
+        }
+    return top+1;
+    }
+}
